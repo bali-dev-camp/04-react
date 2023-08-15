37a4fad8e7c84ca50f3541f005b0b57aac49d890
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2023-08-15 12:08:23 +0800

    refactor: separate navbar styling

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,15 +1,6 @@
 import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import {
-  createStyles,
-  Navbar,
-  Group,
-  getStylesRef,
-  rem,
-  Title,
-  Flex,
-  MediaQuery,
-} from '@mantine/core';
+import { Navbar, Group, Title, Flex, MediaQuery } from '@mantine/core';
 import {
   IconDashboard,
   IconShoe,
@@ -18,61 +9,7 @@ import {
   IconX,
 } from '@tabler/icons-react';
 
-const useStyles = createStyles((theme) => ({
-  header: {
-    paddingBottom: theme.spacing.md,
-    marginBottom: `calc(${theme.spacing.md} * 1.5)`,
-    borderBottom: `${rem(1)} solid ${theme.colors.gray[2]}`,
-  },
-
-  footer: {
-    paddingTop: theme.spacing.md,
-    marginTop: theme.spacing.md,
-    borderTop: `${rem(1)} solid ${theme.colors.gray[2]}`,
-  },
-
-  link: {
-    ...theme.fn.focusStyles(),
-    display: 'flex',
-    alignItems: 'center',
-    textDecoration: 'none',
-    fontSize: theme.fontSizes.sm,
-    color: theme.colors.gray[7],
-    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
-    borderRadius: theme.radius.sm,
-    fontWeight: 500,
-
-    '&:hover': {
-      backgroundColor: theme.colors.gray[0],
-      color: theme.black,
-
-      [`& .${getStylesRef('icon')}`]: {
-        color: theme.black,
-      },
-    },
-  },
-
-  linkIcon: {
-    ref: getStylesRef('icon'),
-    color: theme.colors.gray[6],
-    marginRight: theme.spacing.sm,
-  },
-
-  linkActive: {
-    '&, &:hover': {
-      backgroundColor: theme.fn.variant({
-        variant: 'light',
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-        .color,
-      [`& .${getStylesRef('icon')}`]: {
-        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-          .color,
-      },
-    },
-  },
-}));
+import { useStyles } from './style';
 
 const data = [
   { link: '/', label: 'Dashboard', icon: IconDashboard },

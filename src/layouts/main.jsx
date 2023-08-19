480f6efac2f2b48229bbf8b8f031923b0b47188a
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2023-08-19 12:12:49 +0800

    feat: implement loading state

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,26 +1,35 @@
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import { AppShell, Container } from '@mantine/core';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { AppShell, Container, LoadingOverlay } from '@mantine/core';
 
 import NavbarMain from '../components/navbar';
 import { HeaderMain } from '../components/header';
 
 export default function LayoutMain() {
+  const navigation = useNavigation();
+
   const [opened, setOpened] = useState(false);
 
   return (
-    <AppShell
-      layout="alt"
-      navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
-      navbar={
-        <NavbarMain status={!opened} onToggle={() => setOpened(!opened)} />
-      }
-      header={<HeaderMain onToggle={() => setOpened(!opened)} />}
-    >
-      <Container size="xl">
-        <Outlet />
-      </Container>
-    </AppShell>
+    <>
+      <LoadingOverlay
+        visible={navigation.state === 'loading'}
+        overlayBlur={2}
+      />
+
+      <AppShell
+        layout="alt"
+        navbarOffsetBreakpoint="sm"
+        asideOffsetBreakpoint="sm"
+        navbar={
+          <NavbarMain status={!opened} onToggle={() => setOpened(!opened)} />
+        }
+        header={<HeaderMain onToggle={() => setOpened(!opened)} />}
+      >
+        <Container size="xl">
+          <Outlet />
+        </Container>
+      </AppShell>
+    </>
   );
 }
